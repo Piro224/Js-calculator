dc513ef591cdf75f2a84907c5326e07edee98901
Prosper Azimeh <azimehburna@gmail.com>
2023-01-25 21:57:31 -0800

    Modified

--- calc.js
+++ calc.js
@@ -1,39 +1,39 @@
 
-//CODE TWO....
-//This is more simpler and understanding if you understand the other code
+// //CODE TWO....
+// //This is more simpler and understanding if you understand the other code
 
-(function(){
+// (function(){
      
-    let display = document.querySelector('.textbox');
-    let btns = document.querySelectorAll('.btn');
-    let clear = document.querySelector('.btn-clear');
-    let equals = document.querySelector('.btn-equals');
+//     let display = document.querySelector('.textbox');
+//     let btns = document.querySelectorAll('.btn');
+//     let clear = document.querySelector('.btn-clear');
+//     let equals = document.querySelector('.btn-equals');
 
-    btns.forEach(function(button){
-        button.addEventListener('click', function(e){
-            e.preventDefault();
-            let value = e.target.dataset.num;
-            display.value += value;
+//     btns.forEach(function(button){
+//         button.addEventListener('click', function(e){
+//             e.preventDefault();
+//             let value = e.target.dataset.num;
+//             display.value += value;
            
-        })
-    });
+//         })
+//     });
 
-    equals.addEventListener('click', function(e){
-        e.preventDefault();
-        if(display.value === ''){
-            display.value = "";
-        }else{
-            let answer = eval(display.value);
-            display.value = answer;
-        }
-    });
+//     equals.addEventListener('click', function(e){
+//         e.preventDefault();
+//         if(display.value === ''){
+//             display.value = "";
+//         }else{
+//             let answer = eval(display.value);
+//             display.value = answer;
+//         }
+//     });
 
-    clear.addEventListener('click', function(e){
-        e.preventDefault();
-        display.value = '';
-        // display.value = display.value.slice(0,-1);
-    })
+//     clear.addEventListener('click', function(e){
+//         e.preventDefault();
+//         display.value = '';
+//         // display.value = display.value.slice(0,-1);
+//     })
     
 
-})();
+// })();
 
